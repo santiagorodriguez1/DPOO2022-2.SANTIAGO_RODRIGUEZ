b3b13e558082bbed882ad15d901371a7ae2ee331
santiago <santiago@localhost>
2022-08-19 19:25:02 -0500

    Taller0.SantiagoRodriguez

---
+++ Taller0_Olimpicos/src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -0,0 +1,20 @@
+package uniandes.dpoo.taller0.modificacion;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+
+
+
+public class Modificacion {
+	public static void main(String[]args) throws FileNotFoundException, IOException
+	{
+		CalculadoraEstadisticas calc=LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+		System.out.println(calc.paisConMasMedallistas());
+				
+	}
+
+
+}
+
+
